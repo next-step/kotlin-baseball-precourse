5fb01bb2c4ad64387aeb942dbf2e9a918f0b9b26
sun <50648835+kimseongyu@users.noreply.github.com>
2024-04-26 17:30:08 +0900

    feat: update setRandomNum function in User class
    각 자리 숫자가 1에서 9까지의 서로 다른 임의의 수로 이루어진 3자리 숫자 생성 후
    상대방이 맞출 숫자인 number로 설정

--- src/main/kotlin/User.kt
+++ src/main/kotlin/User.kt
@@ -2,7 +2,13 @@ class User {
     private var _number: String = ""
 
     public fun setRandomNum(): Unit {
-
+        val randomNum = mutableListOf<Int>()
+        while (randomNum.size < 3) {
+            val num = (1..9).random()
+            if (randomNum.contains(num)) continue
+            randomNum.add(num)
+        }
+        _number = randomNum.joinToString("")
     }
 
     public fun compareNum(enemyNum: String): Int {

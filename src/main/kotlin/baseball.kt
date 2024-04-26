1c8139bfb619cfd08674a43ee0010059b96f8299
changukLee <ckddnr5527@gmail.com>
2024-04-27 06:30:22 +0900

    feat: add game repeat feature
    - add game repeat feature

--- src/main/kotlin/baseball.kt
+++ src/main/kotlin/baseball.kt
@@ -2,22 +2,48 @@ import java.io.BufferedReader
 import kotlin.random.Random
 
 fun main() {
-    var goalNumString: String = generateGoalNum()
     val br = BufferedReader(System.`in`.bufferedReader())
-    print("숫자를 입력해 주세요 : ")
-    var inputNumString: String = br.readLine()
-
-    try {
-        checkInputNum(inputNumString)
-    } catch (e: IllegalArgumentException) {
-        println("1부터 9까지 서로 다른 수로 이루어진 3자리의 수를 입력해주세요.")
-        return
-    }
-
+    var goalNumString: String = generateGoalNum()
+    var inputNumString = ""
     val matchResultMap = mutableMapOf("스트라이크" to 0, "볼" to 0, "미스" to 0)
+    var flag = "0" // 0 : 게임 중, 1 : goalNumber 생성 필요, 2 : 게임 종료
+    var gameOver = false
 
-    matchGoalNumAndUserNum(goalNumString, inputNumString, matchResultMap)
-    printMatchResult(matchResultMap)
+    while (flag != "2") {                       // flag가 "2" 일 때까지 게임 반복
+        resetMatchResultMap(matchResultMap)     // resultMap 초기화
+
+        if (flag == "1") {                      // flar가 "1"인 경우 게임을 다시 시작하기 위한 세팅
+            goalNumString = generateGoalNum()
+            flag = "0"
+            gameOver = false
+        }
+
+        print("숫자를 입력해 주세요 : ")            // 유저의 숫자 입력을 받음
+        inputNumString = br.readLine()
+
+        try {                                   // 잘못된 입력을 받은 경우 예외 발생 후 게임 종료
+            checkInputNum(inputNumString)
+        } catch (e: IllegalArgumentException) {
+            println("1부터 9까지 서로 다른 수로 이루어진 3자리의 수를 입력해주세요.")
+            break
+        }
+
+        matchGoalNumAndUserNum(goalNumString, inputNumString, matchResultMap) // 목표와 입력 매칭
+        printMatchResult(matchResultMap)
+
+        if (matchResultMap["스트라이크"] == 3) {   // 숫자를 맞춘 경우 게임종료 메시지 출력, flag 입력 받음
+            printGameOverMessage()
+            flag = br.readLine()
+            gameOver = true
+        }
+
+        try {                                    // flag가 유효한지 확인
+            checkValidFlag(flag, gameOver)
+        } catch (e: IllegalArgumentException) {
+            println("1 또는 2를 입력해주세요.")
+            break
+        }
+    }
 }
 
 /**
@@ -184,4 +210,45 @@ fun printMatchResult(matchResultMap: MutableMap<String, Int>) {
     } else {
         println("${matchResultMap["볼"]}볼 ${matchResultMap["스트라이크"]}스트라이크")
     }
+}
+
+/**
+ * 게임 종료 메시지 출력하는 함수.
+ *
+ * 3 스트라이크일 때 출력
+ */
+fun printGameOverMessage() {
+    println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+}
+
+/**
+ * 플래그의 유효성을 확인하는 함수.
+ *
+ * 게임이 종료되지 않은 경우 확인하지 않음.
+ * 플래그가 "1" 또는 "2"가 아닌 경우 [IllegalArgumentException]을 throw.
+ *
+ * @param flag 확인할 플래그.
+ * @param gameOver 게임이 종료되었는지 여부를 나타내는 플래그.
+ * @throws IllegalArgumentException 플래그가 유효하지 않은 경우.
+ */
+fun checkValidFlag(flag: String, gameOver: Boolean) {
+    if (!gameOver) {
+        return
+    }
+    if (flag != "1" && flag != "2") {
+        throw IllegalArgumentException()
+    }
+}
+
+/**
+ * matchResultMap의 모든 값을 0으로 초기화하는 함수.
+ *
+ * @param matchResultMap 초기화할 MutableMap.
+ *   맵의 키는 "스트라이크", "볼", "미스"이며, 값은 해당 결과의 개수임.
+ */
+fun resetMatchResultMap(matchResultMap: MutableMap<String, Int>) {
+    matchResultMap["스트라이크"] = 0
+    matchResultMap["볼"] = 0
+    matchResultMap["미스"] = 0
 }

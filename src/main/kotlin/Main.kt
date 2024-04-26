b1e86c0346f72f5a5c7ba94cc2297b01d8e2061b
HyeonjiJang <jhg050908@gmail.com>
2024-04-26 21:40:23 +0900

    feat: 1부터 9까지 서로 다른 수로 이루어진 3자리수 생성 기능 구현

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,7 @@ private sealed class GameState {
     data object OnGoing: GameState()
     data object GameEnd: GameState()
 }
+
 private class GameController {
     var gameState: GameState = GameState.BeforeStart
 }
@@ -10,16 +11,28 @@ private class GameController {
 private class Computer {
     val number: Array<Int> = setNumber()
 
-
     private fun setNumber(): Array<Int> {
-        return arrayOf()
+        val number: Array<Int> = Array(3) { 0 }
+        number[0] = getRandomNumber(number)
+        number[1] = getRandomNumber(number)
+        number[2] = getRandomNumber(number)
+
+        return number
     }
 
+    private fun getRandomNumber(nums: Array<Int>): Int {
+        var result = (1..9).random()
+        while (nums.contains(result)) {
+            result = (1..9).random()
+        }
+        return result
+    }
 
     fun printResult() {
 
     }
 }
+
 fun main() {
 
 }

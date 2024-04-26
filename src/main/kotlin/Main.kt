ea469d9e52045b17600addd26aed4ab08b40305a
HyeonjiJang <jhg050908@gmail.com>
2024-04-26 21:34:01 +0900

    feat: Computer 클래스 생성

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,6 +7,19 @@ private class GameController {
     var gameState: GameState = GameState.BeforeStart
 }
 
+private class Computer {
+    val number: Array<Int> = setNumber()
+
+
+    private fun setNumber(): Array<Int> {
+        return arrayOf()
+    }
+
+
+    fun printResult() {
+
+    }
+}
 fun main() {
 
 }

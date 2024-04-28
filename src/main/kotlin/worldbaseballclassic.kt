6fe1ef4d420aad0ce7167b7146701a738b43c029
SHEEN <normengdie@pusan.ac.kr>
2024-04-28 23:51:14 +0900

    add kt file

---
+++ src/main/kotlin/worldbaseballclassic.kt
@@ -0,0 +1,73 @@
+fun main() {
+    while(true){
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        val game = readLine()!!
+        if (game == "1"){
+            startGame(createdRandomNumber())
+
+        } else if (game == "2"){
+            println("프로그램을 종료합니다.")
+            break
+
+        }else{
+            throw IllegalArgumentException("A number must be 1 or 2")
+        }
+    }
+}
+fun baseballGame(checkUserNumber: String, checkRandomNumber : String){ //스트라이크 볼 판정
+    var ball : Int = 0
+    var strike : Int = 0
+    for (i in 0..2){
+        if(checkUserNumber[i] == checkRandomNumber[i]){
+            strike +=1
+        }else if (checkUserNumber[i] != checkRandomNumber[i] && checkUserNumber[i] in checkRandomNumber){
+            ball +=1
+        }else{
+            continue
+        }
+
+    }
+    if(ball ==0 && strike ==0){
+        return println("낫싱")
+    }else if (ball ==0 && strike != 0){
+        return println("${strike}스트라이크")
+    }else if (ball !=0 && strike == 0){
+        return println("${ball}볼")
+    }else{
+        return println("${ball}볼 ${strike}스트라이크")
+    }
+
+}
+
+fun startGame(randomNumberInFunction : String){
+    while(true){
+        println("숫자를 입력해 주세요:")
+        var userNumberInFunction : String = readLine()!!
+        if(randomNumberInFunction == userNumberInFunction){
+            println("3스트라이크")
+            println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+            break
+        }else if(userNumberInFunction.length != 3){
+            throw IllegalArgumentException("A number must be 100~999 ")
+            break
+        }else{
+            baseballGame(userNumberInFunction,randomNumberInFunction)
+        }
+    }
+}
+fun createdRandomNumber() : String { // 랜덤 난수 생성 함수!
+    while(true) {
+        val numberRange = (100..999)
+        val randomNumber = numberRange.random().toString()
+        val numberList = randomNumber.toMutableList()
+        val checkedNumberList = numberList.distinct()
+        if(checkedNumberList.size == 3){ // 숫자 없애기
+            return checkedNumberList.joinToString("")}
+        else{
+            continue
+        }
+    }
+
+}
+
+

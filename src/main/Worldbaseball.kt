6a6f34db002b29c52c200ca9e7d678361ee4b75d
SHEEN <normengdie@pusan.ac.kr>
2024-04-30 20:37:22 +0900

    add randomnumber function contents without overlapping

--- src/main/Worldbaseball.kt
+++ src/main/Worldbaseball.kt
@@ -18,6 +18,16 @@ fun startGame(randomNumberInFunction : String){
 }
 
 fun createdRandomNumber() : String { // 랜덤 난수 생성 함수!
-
-
+    while(true) {
+        val numberRange = (100..999)
+        val randomNumber = numberRange.random().toString()
+        val numberList = randomNumber.toMutableList()
+        val checkedNumberList = numberList.distinct()
+        if(checkedNumberList.size == 3){ // 숫자 없애기
+            return checkedNumberList.joinToString("")}
+        else{
+            continue
+        }
     }
+
+}

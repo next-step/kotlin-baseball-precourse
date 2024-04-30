fc1674fa611fcde4dcdea1bbc7b5c64887f4cf84
SHEEN <normengdie@pusan.ac.kr>
2024-04-30 20:36:53 +0900

    add basic functions structure

---
+++ src/main/Worldbaseball.kt
@@ -0,0 +1,23 @@
+fun main(){
+
+
+
+}
+
+fun baseballGame(checkUserNumber: String, checkRandomNumber : String) { //스트라이크 볼 판정
+
+
+
+
+}
+
+fun startGame(randomNumberInFunction : String){
+
+
+
+}
+
+fun createdRandomNumber() : String { // 랜덤 난수 생성 함수!
+
+
+    }

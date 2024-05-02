8f2062ed32f4ce0f26e8ed5871229f6df5a6f8f1
sumin <cleonn7878@gmail.com>
2024-05-02 21:44:10 +0900

    feat: add runSystem, restartGame
    runSystem:
    - main 함수에서 호출
    - 사용자가 종료를 선택할 때까지 계속해서 새로운 게임을 시작
    restartGame:
    - 사용자에게 재시작, 종료 여부를 입력받는 함수
    - 잘못된 입력을 하였을 때(숫자가 아닐 때) Exception을 발생시킴
    - 1을 입력 받았을 때 true를 반환
    - 2를 입력 받았을 때 false를 반환
    - 이외의 숫자를 입력받으면 1 또는 2를 입력할 때까지 재 입력받음

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,6 +1,7 @@
 fun main() {
-    var answer: List<Int> = createAnswer()
+    runSystem()
 }
+
 // 상대방 기능
 fun createAnswer(): List<Int> {
     val range = (1..9)
@@ -11,4 +12,42 @@ fun createAnswer(): List<Int> {
     val answer: List<Int> = numbers.toList()
     return answer
 }
+
 // 시스템 기능
+fun runSystem() {
+    do {
+        startGame()
+    } while (restartGame())
+    println("애플리케이션을 종료합니다.")
+}
+
+fun startGame() {
+}
+
+fun compareNumber() {
+}
+
+fun checkScore() {
+}
+
+fun restartGame(): Boolean {
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    var restart: Boolean = false
+    var opt: Int = 0
+    try {
+        opt = readln().toInt()
+    }
+    catch (exception: Exception) {
+        throw IllegalArgumentException("잘못된 입력입니다. 애플리케이션을 종료합니다.")
+    }
+
+    if (opt == 1) {
+        restart = true
+    } else if (opt == 2) {
+        restart = false
+    } else {
+        println("1 또는 2를 입력하세요.")
+        restart = restartGame()
+    }
+    return restart
+}

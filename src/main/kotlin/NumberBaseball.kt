77bb9dc8b64817d4c2fd2e8e2a824db9d71d543f
Jeong Suhyeon <jsh00325@naver.com>
2024-05-02 22:38:37 +0900

    feat: add baseball game simulation

---
+++ src/main/kotlin/NumberBaseball.kt
@@ -0,0 +1,14 @@
+fun main() {
+    while (true) {
+        println("===[숫자야구 게임]==========")
+
+        // 게임 객체 생성 -> 생성과 동시에 정답이 정해짐
+        var game: BaseballGame = BaseballGame()
+
+        // 플레이어 객체 생성
+        var player: BaseballPlayer = BaseballPlayer()
+
+
+        break   // 임시
+    }
+}

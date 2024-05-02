83bdc77cee2975d127be776329962835cf8a2432
Jeong Suhyeon <jsh00325@naver.com>
2024-05-02 17:52:42 +0900

    feat: add judge function

--- src/main/kotlin/BaseballGame.kt
+++ src/main/kotlin/BaseballGame.kt
@@ -14,4 +14,48 @@ class BaseballGame() {
         return threeNumberList.joinToString("")
     }
 
+    fun judgeGameResult(player: BaseballPlayer): GameResult {
+        val ballCount: Int = this._ballCheck(player)
+        val strikeCount: Int = this._strikeCheck(player)
+        return GameResult(ballCount, strikeCount)
+    }
+
+    /** 현재 답과 사용자의 입력간의 ball의 개수를 알려주는 내부 함수 */
+    fun _ballCheck(player: BaseballPlayer): Int {
+        var ballCount: Int = 0
+
+        for (i in (0..2)) {
+            // 정답의 i번째 값이 ball에 해당하는지 확인하는 flag 변수
+            var ballFlag: Int = 0
+
+            // player의 j번째 값을 비교
+            for (j in (0..2)) {
+                // ball의 경우 두 위치가 달라야 하므로, i == j인 경우는 제외
+                if (i == j) continue
+
+                // 두 위치의 값이 다른 경우도 해당하지 않으므로 제외
+                if (this.answer[i] != player.inputString[j]) continue
+
+                // ball에 해당하는 경우, ballFlag를 켠다
+                ballFlag = 1
+            }
+
+            ballCount += ballFlag
+        }
+        return ballCount
+    }
+
+    /** 현재 답과 사용자의 입력간의 strike의 개수를 알려주는 내부 함수 */
+    fun _strikeCheck(player: BaseballPlayer): Int {
+        var strikeCount: Int = 0
+
+        // 정답과 palyer의 입력의 i번째 값들이 같은지 확인
+        for (i in (0..2)) {
+            // 각각의 i번째 값이 다른 경우는 strike에 해당하지 않으므로 제외
+            if (this.answer[i] != player.inputString[i]) continue
+
+            strikeCount += 1
+        }
+        return strikeCount
+    }
 }

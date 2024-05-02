89ecdedb2520e4cb493dacec32016e083dcdf335
graccon <sujin.noh01@gmail.com>
2024-05-03 04:18:58 +0900

    feat(game-logic): implement strike and ball determination logic

--- src/main/kotlin/engine/BaseBall.kt
+++ src/main/kotlin/engine/BaseBall.kt
@@ -1,18 +1,19 @@
 package engine
 
+import RandomNumberGenerator
 import engine.io.Input
 import engine.io.NumberGenerator
 import engine.io.Output
 
 class BaseBall constructor(
-    private val generator: NumberGenerator,
+    private val generator: RandomNumberGenerator,
     private val input: Input,
     private val output: Output
 ) : Runnable {
     private val COUNT_OF_NUMBERS: Int = 3
 
     override fun run() {
-        val answer = generator.generate(COUNT_OF_NUMBERS)
+        val answer:List<Int> = generator.generate(COUNT_OF_NUMBERS)
 
         while (true) {
             val inputString: String = input.input("숫자를 입력해 주세요 : ")
@@ -22,7 +23,29 @@ class BaseBall constructor(
                 output.displayInputError()
                 break   // or continue?
             }
+
+            var ballCount = returnBallCount(answer, inputNumber)
+            output.displayBallCount(ballCount)
+
+            // 스트라이크 및 볼 판정 로직
         }
+    }
+    fun Hello(){
+
+    }
+    fun returnBallCount(answer:List<Int>, inputNumbers:List<Int>): BallCount {
+        var strikes = 0
+        var balls = 0
+
+        answer.forEachIndexed { i, n ->
+            inputNumbers.forEachIndexed {j, m ->
+                if (n == m) {
+                    if (i == j) strikes++
+                    else balls++
+                }
+            }
+        }
+        return BallCount(strikes, balls)
     }
 
     fun parse(inputString: String): List<Int>? {

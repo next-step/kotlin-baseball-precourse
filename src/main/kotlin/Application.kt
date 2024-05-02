8be8ddf95735c7bb75a9737a3adeac7ad3037c5d
Jeong hoon <hoondb@naver.com>
2024-05-02 15:55:01 +0900

    feat: Add check input number feature
    - add check input number feature

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,4 +1,6 @@
 import kotlin.random.Random
+import java.io.BufferedReader
+
 
 fun generateGoalNumber(): String {
     var number = mutableSetOf<Int>()
@@ -10,4 +12,21 @@ fun generateGoalNumber(): String {
     return number.joinToString("")
 }
 
-fun main() {}
+fun checkInputNumber(number: String){
+    //세 자리 숫자인지 확인
+    if (number.length != 3){
+        throw IllegalArgumentException("세 자리 숫자를 입력해주세요")
+    }
+    // 각 자리 숫자가 1~9인지 확인
+    number.forEach{
+        if (it !in '1'..'9'){
+            throw IllegalArgumentException("각 자리의 숫자는 1~9사이 입니다")
+        }
+    }
+}
+
+fun compareNumber(){}
+
+fun main(){
+    
+}

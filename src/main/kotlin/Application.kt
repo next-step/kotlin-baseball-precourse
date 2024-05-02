e861fa9c7bbfa294fa2817fe102b3e6deb84b890
khyeonm <122lamb@naver.com>
2024-05-02 23:04:54 +0900

    feat: Make playGame

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,3 +1,5 @@
+import kotlin.random.Random
+
 fun main() {
     var playAgain = true
     while (playAgain) {
@@ -8,6 +10,29 @@ fun main() {
     println("게임이 종료되었습니다.")
 }
 
+fun playGame (computerNumbers: List<Int>) {
+    var gameOver = false
+    while (!gameOver) {
+        val userInput = getUserInput()
+        val userNumbers = parseUserInput(userInput)
+        if (!isValidInput(userNumbers)) {
+            throw new IllegalArgumentException("애플리케이션 종료")
+        }
+        val (strikes, balls) = calculateResult(computerNumbers, userNumbers)
+        if (strikes == 3) {
+            println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+            gameOver = true
+        } else if (strikes != 0 || balls != 0){
+            printGameResult(balls, strikes)
+        } else {
+            println("낫싱")
+        }
+    }
+}
 fun generateComputerNumbers(): List<Int> {}
-fun playGame (computerNumbers: List<Int>) {}
-fun askForAnotherGame() : Int {}
+fun getUserInput(): String {}
+fun isValidInput(numbers: List<Int>):
+fun parseUserInput(input: String):
+fun calculateResult(computerNumbers: List<Int>, userNumbers: List<Int>):
+fun printGameResult(balls: Int, strikes: Int) {}
+fun askForAnotherGame() : Boolean {}

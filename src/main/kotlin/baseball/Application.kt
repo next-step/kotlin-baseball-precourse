b27f748f4a8bd83025e1c7a911bf61557b57d364
njiyeon <chxistinn@gmail.com>
2024-05-04 01:24:31 +0900

    application

---
+++ src/main/kotlin/baseball/Application.kt
@@ -0,0 +1,11 @@
+package baseball
+
+import baseball.game.BaseballGame
+import baseball.game.Game
+
+private val baseballGame: Game by lazy {
+    BaseballGame()
+}
+fun main() {
+    baseballGame.start()
+}

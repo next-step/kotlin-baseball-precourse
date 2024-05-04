87e4ce624c825b8d72b7c8ebf6aebb747a52bd5e
sumin <cleonn7878@gmail.com>
2024-05-04 16:35:27 +0900

    test: add test for getInput, compareNumber, printScore,restartGame
    getInput:
    - 정수로 변환이 가능한지 않은 입력은 IllegalArgumentException을 발생시키는지 확인
    - 정수로 변환이 가능한 입력은 정상적으로 변환이 수행되었는지 확인
    
    compareNumber:
    - 예시 정답에 대해 다양한 예측값에 대해 올바르게 점수를 매기는지 확인
    
    printScore:
    - 매겨진 점수를 올바른 형식으로 출력하는지 확인
    - 낫싱인 경우, 볼만 있는 경우, 스트라이크만 있는 경우, 둘다 있는 경우로 모든 케이스에 대해 시행
    
    restartGame:
    - 1을 입력하면 true, 2를 입력하면 false로 반환하는지 확인
    
    setTestScore, setTestAnswer:
    - 원활한 테스트 수행을 위해 특정상황을 설정할 수 있도록 하는 함수 구현

--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -1,13 +1,14 @@
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.io.ByteArrayInputStream
+
 class ApplicationTest {
 
     lateinit var gameInfo: GameInfo
     @BeforeEach
     fun setUp() {
         gameInfo = GameInfo(createAnswer())
-
     }
 
     @Test
@@ -19,23 +20,83 @@ class ApplicationTest {
         assertThat(answer.toSet().size).isEqualTo(3)
     }
     @Test
-    fun `유효한 예측은 각 자릿수가 1에서 9사이의 값을 가지는 3자리 수`() {
+    fun `유효하지 않은 입력(정수형이 아닌 입력)은 IllegalArgumentException를 발생시킴`() {
+        var inputStream = ByteArrayInputStream("a".toByteArray())
+        System.setIn(inputStream)
+        assertThatThrownBy { getInput() }.isInstanceOf(IllegalArgumentException::class.java)
+
+        inputStream = ByteArrayInputStream("1.2".toByteArray())
+        System.setIn(inputStream)
+        assertThatThrownBy { getInput() }.isInstanceOf(IllegalArgumentException::class.java)
+
+        inputStream = ByteArrayInputStream("일".toByteArray())
+        System.setIn(inputStream)
+        assertThatThrownBy { getInput() }.isInstanceOf(IllegalArgumentException::class.java)
+
+        inputStream = ByteArrayInputStream("1".toByteArray())
+        System.setIn(inputStream)
+        assertThat(getInput()).isEqualTo(1)
     }
     @Test
     fun `자릿수가 같으면 스트라이크, 다르면 볼, 아예 포함하고 있지않으면 아무것도 하지않음`() {
+        setTestAnswer(1,2,3)
+        var predict = listOf(1,4,5)
+        compareNumber(predict, gameInfo)
+        assertThat(gameInfo.ball).isEqualTo(0)
+        assertThat(gameInfo.strike).isEqualTo(1)
 
-    }
-    @Test
-    fun `3스트라이크이면 게임을 종료함`() {
-        gameInfo.clearScore()
-        gameInfo.strike = 3
-        assertThat(checkEnd(gameInfo)).isTrue()
+        predict = listOf(4,3,2)
+        compareNumber(predict, gameInfo)
+        assertThat(gameInfo.ball).isEqualTo(2)
+        assertThat(gameInfo.strike).isEqualTo(0)
+
+        predict = listOf(1,3,2)
+        compareNumber(predict, gameInfo)
+        assertThat(gameInfo.ball).isEqualTo(2)
+        assertThat(gameInfo.strike).isEqualTo(1)
+
+        predict = listOf(7,8,9)
+        compareNumber(predict, gameInfo)
+        assertThat(gameInfo.ball).isEqualTo(0)
+        assertThat(gameInfo.strike).isEqualTo(0)
+
+        predict = listOf(1,2,3)
+        compareNumber(predict, gameInfo)
+        assertThat(gameInfo.ball).isEqualTo(0)
+        assertThat(gameInfo.strike).isEqualTo(3)
     }
     @Test
     fun `0볼 0스트라이크 인 경우 낫싱 출력, 이외의 경우 0이 아닌 스트라이크와 볼의 개수 출력`() {
+        setTestScore(0,0)
+        assertThat(printScore(gameInfo)).isEqualTo("낫싱")
+        setTestScore(2,0)
+        assertThat(printScore(gameInfo)).isEqualTo("2볼 ")
+        setTestScore(0,2)
+        assertThat(printScore(gameInfo)).isEqualTo("2스트라이크")
+        setTestScore(1,2)
+        assertThat(printScore(gameInfo)).isEqualTo("1볼 2스트라이크")
+    }
+    @Test
+    fun `3스트라이크이면 게임을 종료함`() {
+        setTestScore(0,3)
+        assertThat(checkEnd(gameInfo)).isTrue()
     }
     @Test
     fun `게임이 끝났을 때 1을 입력하면 새로 시작, 2를 입력하면 종료`() {
+        var inputStream = ByteArrayInputStream("1".toByteArray())
+        System.setIn(inputStream)
+        assertThat(restartGame()).isEqualTo(true)
 
+        inputStream = ByteArrayInputStream("2".toByteArray())
+        System.setIn(inputStream)
+        assertThat(restartGame()).isEqualTo(false)
+    }
+    fun setTestScore(ball:Int, strike: Int) {
+        gameInfo.ball = ball
+        gameInfo.strike = strike
+    }
+
+    fun setTestAnswer(num1: Int, num2: Int, num3: Int) {
+        gameInfo.answer = listOf(num1, num2, num3)
     }
 }

aefe6a78bd57476767b70598399bc2ecc8edea86
ajang369 <jhjun99421@gmail.com>
2024-05-04 21:39:30 +0900

    feat : implement input number
    숫자 입력 후 각 자리 숫자를 리스트 형식으로 변환.

--- src/main/kotlin/Baseball.kt
+++ src/main/kotlin/Baseball.kt
@@ -1,16 +1,26 @@
 import kotlin.random.Random
 
 fun main() {
-    val answerNumList = setRandomNumber()
-
+    val answerNumList = setRandomNumber(3)
+    val inputNumList = inputNumber()
 }
 
-fun setRandomNumber() : List<Int> {
+// 세자리 랜덤 숫자 생성
+fun setRandomNumber(length: Int) : List<Int> {
     val randomNum = mutableListOf<Int>()
-    while (randomNum.size < 3) {
+    while (randomNum.size < length) {
         val num = Random.nextInt(1,9)
         if (!randomNum.contains(num))
             randomNum.add(num)
     }
     return randomNum
+}
+
+// 숫자 입력
+fun inputNumber() : MutableList<Int> {
+    print("숫자를 입력해주세요 : ")
+    val inputNum = readlnOrNull() ?: ""
+    val inputNumList = inputNum.map { it.toString().toInt() }.toMutableList()
+
+    return inputNumList
 }

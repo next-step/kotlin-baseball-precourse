13dcef88ca60f9260820394f8afa657d82469369
todlf <todlfdl@naver.com>
2024-05-05 16:03:12 +0900

    볼,스트라이크, 사용자 입력 구현
    사용자에게 입력을 받아서 list<Int>형태로 만들고, ball과 strike를 체크하는 함수를 만듬

--- 숫자야구/코드.kts
+++ 숫자야구/코드.kts
@@ -1,15 +1,61 @@
-import java.util.Random
+//import java.util.Random
+import java.util.*
 
 val random = Random()
-val numberList = mutableListOf<Int>()
+val comNumberList = mutableListOf<Int>()
 
-while (true) {
-    val computerNum = random.nextInt(9)+1
-    if (!numberList.contains(computerNum)) {
-        numberList.add(computerNum)
-    }
-    if (numberList.size > 2) break
+
+fun plusNumbers(first_num: Int, second_num: Int): Int {
+    val result: Int = first_num + second_num
+    return result
 }
 
-print(numberList)
+fun makeRanNum() {
+    while (true) {
+        val computerNum = random.nextInt(9) + 1
+        if (!comNumberList.contains(computerNum)) {
+            comNumberList.add(computerNum)
+        }
+        if (comNumberList.size > 2) break
+    }
+}
+// 컴퓨터가 랜덤한 숫자 3개 생성 후 리스트에 저장
+var strike: Int = 0
+var ball: Int = 0
+fun checkStrike(userNumberList: List<Int>): Unit {
+    strike = 0
+    if (comNumberList[0] == userNumberList[0])
+        strike++
+    if (comNumberList[1] == userNumberList[1])
+        strike++
+    if (comNumberList[2] == userNumberList[2])
+        strike++
+}
+
+fun checkBall(userNumberList: List<Int>): Unit {
+    ball = 0
+    for (i in comNumberList) {
+        if (i == userNumberList[0])
+            ball++
+        if (i == userNumberList[1])
+            ball++
+        if (i == userNumberList[2])
+            ball++
+    }
+}
+
+//print(numberList)
+fun main() {
+    makeRanNum()
+    println("컴퓨터가 생성한 숫자 : $comNumberList")
+
+    print("숫자를 입력해 주세요 : ")
+    val input = readLine()
+    var userNumberList = input?.map { it.toString().toInt() } ?: listOf()
+    checkStrike(userNumberList)
+    checkBall(userNumberList)
+    println("${ball- strike} 볼 $strike 스트라이크 입니다.")
+}
+
+main()
 

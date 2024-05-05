faf04a6aab5761913f5aa96ea653397c08361085
유지예 <jiyeyu@yujiyeui-MacBookAir.local>
2024-05-06 04:55:04 +0900

    사용자 입력 검증 기능 구현

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -16,12 +16,17 @@ class BaseballGame {
                 print("숫자를 입력해 주세요 : ")
                 val userInput = readLine()
                 try {
-
-                } catch() {
-
-
+                    val result = checkInput(userInput, targetNumbers) // targetNumbers 전달
+                    println(result)
+                    if (result == "3스트라이크") {
+                        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+                        isGameEnd = true
+                    }
+                } catch (e: IllegalArgumentException) {
+                    return
                 }
             }
+
             print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ")
             val restartInput = readLine()
             isRestarting = restartInput?.trim() == "1"

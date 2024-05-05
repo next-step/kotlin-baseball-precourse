42a0861a73a5879577bc6f8c0dd42859ef80a9cf
KSC <ksc1008@naver.com>
2024-05-05 17:53:43 +0900

    feat(BaseballGame): BaseballGame class
    Implemented BaseballGuessResult data class:
    - represents GuessResult derived in BaseballGame.guess
    - contains balls:Int and strikes:Int
    
    Implemented BaseballGame class:
    - contains the answer of the current game and responsibility to proceed game by providing guess and compare method
    - gets a ThreeDigits as a constructor
    - all three digits of the constructor parameter should be unique
    - throws IllegalArgumentException if input ThreeDigits contains any identical digits
    
    Implemented BaseballGame.guess method:
    - compare the input with the answer and returns balls and strikes count
    - receives ThreeDigits as an input
    - returns a BaseballGameResult

---
+++ src/main/kotlin/BaseballGame.kt
@@ -0,0 +1,45 @@
+class BaseballGame(private val answer:ThreeDigits) {
+    private fun checkIfDigit(numberToCheck:Int):Boolean{
+        return (numberToCheck < 10) and (numberToCheck >= 0)
+    }
+    private fun checkBall(numberToFind:Int, digit:Int):Boolean{
+        if(!checkIfDigit(numberToFind))
+            throw IllegalArgumentException("input number must be a digit")
+        if((digit<0) or (digit>=3))
+            throw IllegalArgumentException("input digit must between [0-2]")
+        for(i in 0..2){
+            if((answer[i] == numberToFind) and (i != digit))
+                return true
+        }
+        return false
+    }
+
+    private fun checkStrike(numberToFind: Int, digit: Int):Boolean{
+        if(!checkIfDigit(numberToFind))
+            throw IllegalArgumentException("input number must be a digit")
+        if((digit<0) or (digit>=3))
+            throw IllegalArgumentException("input digit must between [0-2]")
+        if(answer[digit] == numberToFind)
+            return true
+        return false
+    }
+
+    fun guess(input:ThreeDigits):BaseballGuessResult{
+        var strikes = 0
+        var balls = 0
+        for(i in 0..2){
+            if(checkStrike(input[i],i))
+                strikes++
+            else if(checkBall(input[i],i))
+                balls++
+        }
+        return BaseballGuessResult(strikes,balls)
+    }
+
+    init{
+        if((answer.digit1 == answer.digit2)
+            or (answer.digit2 == answer.digit3)
+            or (answer.digit1 == answer.digit3))
+            throw IllegalArgumentException("the correct answer to the game should not contain duplicates")
+    }
+}
---
+++ src/test/kotlin/BaseballGameTest.kt
@@ -0,0 +1,35 @@
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class BaseballGameTest {
+    @ParameterizedTest(name = "BaseballGame([{0}, {1}, {2}]) should throw illegalArgumentException")
+    @CsvSource(
+        "1, 2, 2",
+        "4, 4, 4",
+        "1, 0, 1",
+        "3, 3, 2")
+    fun test_baseballGame_init(param1:Int, param2:Int, param3:Int){
+        fun test_threeDigits_if_given_argument_out_of_bound(param1:Int, param2:Int, param3:Int) {
+            Assertions.assertThatIllegalArgumentException().isThrownBy {
+                BaseballGame(ThreeDigits(param1, param2, param3))
+            }
+        }
+    }
+
+    @ParameterizedTest(name = "guessing ({3}, {4}, {5}) to answer ({0}, {1}, {2}) should be [balls = {6}, strikes = {7}]")
+    @CsvSource(
+        "1, 2, 3, 1, 2, 3, 0, 3",
+        "4, 5, 6, 3, 5, 7, 0, 1",
+        "1, 2, 3, 3, 1, 2, 3, 0",
+        "7, 0, 9, 0, 4, 5, 1, 0",
+        "4, 3, 2, 5, 6, 7, 0, 0",
+        "9, 8, 0, 8, 5, 0, 1, 1")
+    fun test_baseballGame_guess(gameParam1:Int, gameParam2:Int, gameParam3:Int, guessParam1:Int, guessParam2: Int, guessParam3:Int, expectedBalls:Int, expectedStrikes:Int){
+        val game = BaseballGame(ThreeDigits(gameParam1, gameParam2, gameParam3))
+        assertThat(game.guess(ThreeDigits(guessParam1, guessParam2, guessParam3))).matches {
+            (it.balls == expectedBalls) and (it.strikes == expectedStrikes)
+        }
+    }
+}

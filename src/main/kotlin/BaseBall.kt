f1a78b4d2de058ae2e5b38a4d5fcf68ba92a6a25
LeeMinSeo <real@naver.com>
2024-05-06 05:23:38 +0900

    feat : player input 3 numbers and exception case

--- src/main/kotlin/BaseBall.kt
+++ src/main/kotlin/BaseBall.kt
@@ -2,3 +2,13 @@
 fun randomNumber(): List<Int> {
     return (1..9).shuffled().take(3)
 }
+
+//플레이어: 3개 숫자 입력
+fun playerInput(): List<Int> {
+    print("숫자를 입력해 주세요 : ")
+    val input = readLine() ?: throw IllegalArgumentException("입력이 없습니다")
+    if(input.length != 3 || !input.all {it.isDigit()} || input.toSet().size != 3) {
+        throw IllegalArgumentException("1~9까지 서로 다른 수로 이루어진 3자리의 숫자를 입력하세요.")
+    }
+    return input.map {it.toString().toInt()}
+}

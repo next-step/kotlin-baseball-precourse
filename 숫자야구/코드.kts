e72e356a7572dbd840d65c8cdef9e693c37dd2f8
todlf <todlfdl@naver.com>
2024-05-05 16:40:54 +0900

    낫싱과 다시 시작 추가
    낫싱을 if문으로 추가했고, restart 변수를 만들어 1번 누르면 다시 main함수를 호출하고 2번 누르면 return으로 끝내게 만듬

--- 숫자야구/코드.kts
+++ 숫자야구/코드.kts
@@ -1,16 +1,19 @@
 //import java.util.Random
 import java.util.*
 
-val random = Random()
 val comNumberList = mutableListOf<Int>()
-
+var strike: Int = 0
+var ball: Int = 0
+val Restart: Int = 0
 
 fun plusNumbers(first_num: Int, second_num: Int): Int {
     val result: Int = first_num + second_num
     return result
 }
 
-fun makeRanNum() {
+fun makeRanNum() { // 컴퓨터가 랜덤한 숫자 3개 생성 후 리스트에 저장
+    val random = Random()
+    comNumberList.clear()
     while (true) {
         val computerNum = random.nextInt(9) + 1
         if (!comNumberList.contains(computerNum)) {
@@ -19,9 +22,8 @@ fun makeRanNum() {
         if (comNumberList.size > 2) break
     }
 }
-// 컴퓨터가 랜덤한 숫자 3개 생성 후 리스트에 저장
-var strike: Int = 0
-var ball: Int = 0
+
+
 fun checkStrike(userNumberList: List<Int>): Unit {
     strike = 0
     if (comNumberList[0] == userNumberList[0])
@@ -44,18 +46,36 @@ fun checkBall(userNumberList: List<Int>): Unit {
     }
 }
 
+fun numberTry(): Unit {
+    print("숫자를 입력해 주세요 : ")
+    val input = readLine()
+    val userNumberList = input?.map { it.toString().toInt() } ?: listOf()
+    checkStrike(userNumberList)
+    checkBall(userNumberList)
+    if (strike == 0 && ball == 0) println("낫싱") else {
+        println("${ball - strike} 볼 $strike 스트라이크")
+    }
+}
+
 //print(numberList)
 fun main() {
     makeRanNum()
     println("컴퓨터가 생성한 숫자 : $comNumberList")
+    while (true) {
+        numberTry()
+        if (strike == 3) {
+            println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+            println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+            break
+        }
+    }
+    val reStart = readLine()!!.toInt()
+    if (reStart == 1) main()
+    if (reStart == 2) return
 
-    print("숫자를 입력해 주세요 : ")
-    val input = readLine()
-    var userNumberList = input?.map { it.toString().toInt() } ?: listOf()
-    checkStrike(userNumberList)
-    checkBall(userNumberList)
-    println("${ball- strike} 볼 $strike 스트라이크 입니다.")
+
 }
 
 main()
+
 

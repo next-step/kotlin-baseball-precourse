46333d8a53e8f11beb90e38760f6bb1fb09f03fd
DeveloperBaa <developer.baa.code@gmail.com>
2024-05-06 07:25:49 +0900

    - 정답을 맞출 때까지 게임 계속하기

--- untitled/src/main/Application.kt
+++ untitled/src/main/Application.kt
@@ -1,63 +1,57 @@
 import kotlin.random.Random
 
 fun main() {
-    val tmp = mutableListOf<Int>()
-    while (tmp.size < 3) {
-        tmp.add(Random.nextInt(1, 10))
+    val secretNumber = generateSecretNumber()
+
+    while (true) {
+        playGame(secretNumber)
+        if (!playAgain()) break
     }
-    val tmpArray = tmp.toIntArray()
-
-    println("세 개의 숫자를 입력하세요(123처럼 빈칸 없이): ")
-    val input = readLine()
-    val inputNum = mutableListOf<Int>()
-
-    if (input == null || input.length != 3) {
-        throw IllegalArgumentException("올바른 입력값이 아닙니다!")
-    } else {
-        for (char in input) {
-            val num = char.toString().toInt()
-            inputNum.add(num)
-        }
-    }
-
-    check(inputNum.toIntArray(), tmpArray)
 }
 
-fun check(guess: IntArray, answer: IntArray) {
-    var strike = 0
-    var ball = 0
-
-    for (i in guess.indices) {
-        if (guess[i] == answer[i]) {
-            strike++
-        } else if (guess.contains(answer[i])) {
-            ball++
-        }
+fun generateSecretNumber(): IntArray {
+    val secret = mutableListOf<Int>()
+    while (secret.size < 3) {
+        secret.add(Random.nextInt(1, 10))
     }
+    return secret.toIntArray()
+}
+
+fun playGame(secretNumber: IntArray) {
+    println("세 개의 숫자를 입력하세요(123처럼 빈칸 없이): ")
+    val input = readLine()
+    val guess = input?.let {
+        if (it.length != 3) throw IllegalArgumentException("올바른 입력값이 아닙니다!")
+        it.map { char -> char.toString().toInt() }.toIntArray()
+    } ?: throw IllegalArgumentException("올바른 입력값이 아닙니다!")
+
+    val (strike, ball) = checkGuess(guess, secretNumber)
 
     if (strike == 3) {
         println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
-        exit()
-    } else if (strike > 0 && ball > 0) {
-        println("$strike 스트라이크, $ball 볼")
-    } else if (strike > 0) {
-        println("$strike 스트라이크")
-    } else if (ball > 0) {
-        println("$ball 볼")
+        playAgain()
     } else {
-        println("낫싱")
+        if (strike > 0) println("$strike 스트라이크")
+        if (ball > 0) println("$ball 볼")
+        if (strike == 0 && ball == 0) println("낫싱")
     }
 }
 
-fun exit() {
-    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-    val choice = readLine()
-    when (choice) {
-        "1" -> main()
-        "2" -> return
-        else -> {
-            println("잘못된 입력입니다.")
-            exit()
+fun checkGuess(guess: IntArray, secretNumber: IntArray): Pair<Int, Int> {
+    var strike = 0
+    var ball = 0
+    for (i in guess.indices) {
+        if (guess[i] == secretNumber[i]) {
+            strike++
+        } else if (guess.contains(secretNumber[i])) {
+            ball++
         }
     }
+    return Pair(strike, ball)
+}
+
+fun playAgain(): Boolean {
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    val choice = readLine()
+    return choice == "1"
 }

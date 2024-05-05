c095671cd3191603907505a7d7512a3075ec8f3a
유지예 <jiyeyu@yujiyeui-MacBookAir.local>
2024-05-06 04:54:09 +0900

    게임실행기본기능 구현

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,4 +5,26 @@ fun main() { //기본 게임 구현
 
 class BaseballGame {
     val targetNumbers: List<Int> = generateTargetNumbers()
+
+    fun play() {
+        var isRestarting = true
+
+        while (isRestarting) {
+            var isGameEnd = false
+
+            while (!isGameEnd) {
+                print("숫자를 입력해 주세요 : ")
+                val userInput = readLine()
+                try {
+
+                } catch() {
+
+
+                }
+            }
+            print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ")
+            val restartInput = readLine()
+            isRestarting = restartInput?.trim() == "1"
+        }
+    }
 }

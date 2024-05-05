9008a84d69c09cbe5794a4307da5bc1cd7fb8094
nJiyeon <150491973+nJiyeon@users.noreply.github.com>
2024-05-05 11:07:20 +0900

    Update ExceptionHandler.kt

--- src/main/kotlin/utils/ExceptionHandler.kt
+++ src/main/kotlin/utils/ExceptionHandler.kt
@@ -1,38 +1,36 @@
 package utils
 
-import utils.Constraints.DIGIT_LENGTH
-
 object ExceptionHandler {
     fun validateLengthOrException(answer: String, expectedLength: Int) {
-        if (answer.length != expectedLength) throw IllegalArgumentException(Constraints.LENGTH_MISMATCH_EXCEPTION)
+        if (answer.length != expectedLength) throw IllegalArgumentException("입력값의 길이가 올바르지 않습니다.")
     }
 
     fun <E> validateListLengthOrException(list: List<E>, expectedLength: Int) {
-        if (list.size != expectedLength) throw IllegalArgumentException(Constraints.LENGTH_MISMATCH_EXCEPTION)
+        if (list.size != expectedLength) throw IllegalArgumentException("입력값의 길이가 올바르지 않습니다.")
     }
 
     fun validateDigitOrException(ch: Char): Int {
-        val digit = ch.digitToIntOrNull() ?: throw IllegalArgumentException(Constraints.NON_DIGIT_EXCEPTION)
+        val digit = ch.digitToIntOrNull() ?: throw IllegalArgumentException("입력값이 숫자가 아닙니다.")
         validateRangeOrException(digit)
 
         return digit
     }
 
     fun validateRangeOrException(digit: Int) {
-        if (digit !in Constraints.MIN_DIGIT..Constraints.MAX_DIGIT) throw IllegalArgumentException(Constraints.OUT_OF_RANGE_EXCEPTION)
+        if (digit !in 1..9) throw IllegalArgumentException("입력값이 올바른 범위가 아닙니다.")
     }
 
     fun validateDuplicatedOrException(numbers: List<Int>) {
         val notDuplicatedNumbers = numbers.toSet()
-        if (notDuplicatedNumbers.size != DIGIT_LENGTH) throw IllegalArgumentException(Constraints.DUPLICATED_DIGIT_EXCEPTION)
+        if (notDuplicatedNumbers.size != 3) throw IllegalArgumentException("중복된 숫자가 존재합니다.")
     }
 
     fun validateRestartCode(code: String) {
-        validateLengthOrException(code, Constraints.RESTART_LENGTH)
+        validateLengthOrException(code, 1)
         validateRestartCodeCategory(code)
     }
 
     fun validateRestartCodeCategory(code: String) {
-        if (code != Constraints.YES_RESTART && code != Constraints.NO_RESTART) throw IllegalArgumentException(Constraints.WRONG_FORMAT_EXCEPTION)
+        if (code != "1" && code != "2") throw IllegalArgumentException("잘못된 입력 형식입니다.")
     }
 }

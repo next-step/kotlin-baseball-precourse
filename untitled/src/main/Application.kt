ee5a1308ce7d511619a62ad5f283b655c3ec423f
DeveloperBaa <developer.baa.code@gmail.com>
2024-05-06 05:55:38 +0900

    3자리 수 입력받기

--- untitled/src/main/Application.kt
+++ untitled/src/main/Application.kt
@@ -7,5 +7,18 @@ fun main() {
     }
 
     val answer = tmp.joinToString("").toInt()
-    print(answer)
+    //print(answer)
+
+    val inputNum = mutableListOf<Int>()
+
+    println("세 개의 숫자를 입력하세요(123처럼 빈칸 없이): ")
+    val input = readLine()
+
+    if (input != null && input.length == 3){
+        for (char in input) {
+            val num = char.toString().toInt()
+            inputNum.add(num)
+        }
+    }
+    //print(inputNum)
 }

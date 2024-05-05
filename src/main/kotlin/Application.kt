bb56b7f8f683a5ff65d84fff8bbc1840b3f1d189
kimdo-7275 <66244658+kimdo-7275@users.noreply.github.com>
2024-05-05 15:43:33 +0900

    baseball game start

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -31,3 +31,34 @@ fun restartCheck(): Int {
     if (restartInt != 1 && restartInt != 2) throw IllegalArgumentException("IllegalArgumentException")
     return restartInt
 }
+
+fun gameStart() {
+    val gameRadnomNumber = selectRandomnumber()
+
+    while (true) {
+        var strike: Int = 0
+        var ball: Int = 0
+        val playingNumber = checkInputNumber()
+        if (playingNumber[0].toString() == gameRadnomNumber[0]) strike += 1
+        else if (playingNumber[0].toString() == gameRadnomNumber[1] || playingNumber[0].toString() == gameRadnomNumber[2]) ball += 1
+        if (playingNumber[1].toString() == gameRadnomNumber[1]) strike += 1
+        else if (playingNumber[1].toString() == gameRadnomNumber[0] || playingNumber[1].toString() == gameRadnomNumber[2]) ball += 1
+        if (playingNumber[2].toString() == gameRadnomNumber[2]) strike += 1
+        else if (playingNumber[2].toString() == gameRadnomNumber[1] || playingNumber[2].toString() == gameRadnomNumber[0]) ball += 1
+
+        if (strike == 0 && ball == 0) println("낫싱")
+        else if (strike == 0) println("" + ball + "볼")
+        else if (ball == 0) println("" + strike + "스트라이크")
+        else println("" + ball + "볼 " + strike + "스트라이크")
+        if (strike == 3) println("3개의 숫자를 모주 맞히셨습니다! 게임 종료\n" + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+        if (strike == 3) break
+    }
+    val restart = try {
+        restartCheck()
+    } catch (exception: IllegalArgumentException) {
+        println("IllegalArgumentException")
+        System.exit(1)
+    }
+    if (restart == 2) return
+    else if (restart == 1) gameStart()
+}

d93b770d5b56873830a455fa49eaf7a8dc1670ee
Lorenzo <tnalsdlekgsk1@icloud.com>
2024-05-06 02:49:34 +0900

    feat(application) 입력받아 숫자로 변환하는 기능 구현

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,38 +1,63 @@
 fun main() {
-    while (true) {
+    while(true){
         run()
     }
 }
-
-fun checkInt(numbers: String): Boolean { // 숫자인지 확인
-    try {
-        var numbers = numbers.toInt()
-        return true
-    } catch(e: IllegalArgumentException) {
-        throw  IllegalArgumentException("잘못된 입력입니다. 프로그램을 종료합니다.")
+fun run() {
+    while(true){
+        try {
+            playGame()
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            break
+        }
     }
 }
-
-
 fun generateRandomInt(): IntArray { // 서로 다른 수로 구성된 배열 리턴
     val answer = IntArray(3)
     for (i in 0 until 3) {
-        var randomNumber: Int
-        do {
-            randomNumber = (1..9).random() // 1부터 9 사이의 임의의 숫자 생성
-        } while (randomNumber in answer) // 이미 사용된 숫자인 경우 다시 생성
+        var randomNumber = getUniqueNumber(answer)
         answer[i] = randomNumber
     }
     return answer
 }
-
-fun run() { // 숫자야구 실행
-    val answer = generateRandomInt()
-
+fun getUniqueNumber(answer: IntArray): Int {
+    var number : Int
+    do {
+        number = (1..9).random() // 1부터 9 사이의 임의의 숫자 생성
+    } while (number in answer)
+    return number
 }
-
-fun playGame() {
-
+fun playGame() { // 숫자야구 실행한다
+    val answer = generateRandomInt()
+    while(true){
+        val userInput = getUserInput()
+        println(userInput.joinToString { "" })
+    }
+}
+fun getUserInput(): IntArray {
+    print("숫자를 입력해 주세요: ")
+    val input = readLine() ?: throw IllegalArgumentException("잘못된 입력입니다.")
+    checkInputLength(input)
+    checkInputIsNumeric(input)
+    checkInputDuplicate(input)
+    return stringToNumberArray(input) // 검사 통과하면 유저 배열을 반환한다
+}
+fun checkInputLength(input: String) {
+    if (input.length != 3) {
+        throw IllegalArgumentException("세 자리가 아닙니다.")
+    }
+}
+fun checkInputIsNumeric(input: String) {
+    if (!input.all { it.isDigit() }) {
+        throw IllegalArgumentException("숫자가 아닙니다.")
+    }
+}
+fun checkInputDuplicate(input: String) {
+    val distinctNumbers = input.toSet() // set으로 변환해 중복 제거
+    if (distinctNumbers.size != 3) {
+        throw IllegalArgumentException("중복된 숫자가 있습니다.")
+    }
 }
 fun stringToNumberArray(input: String): IntArray {
     val numbers = IntArray(3)
@@ -42,7 +67,6 @@ fun stringToNumberArray(input: String): IntArray {
     }
     return numbers
 }
-
 fun checkAnswer(){
 
 }

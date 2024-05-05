79f94675b6dd9324a8319effc265d2a4fafc2944
유지예 <jiyeyu@yujiyeui-MacBookAir.local>
2024-05-06 04:59:05 +0900

    입력 정상 처리 구현

---
+++ src/test/kotlin/BaseballGameTest.kt
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class BaseballGameTest {
+
+}

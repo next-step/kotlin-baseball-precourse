a8e28246ad51bc9264f916a7eb9ea79b8da4b985
DeveloperBaa <developer.baa.code@gmail.com>
2024-05-06 06:56:30 +0900

    - 오류 수정1

--- untitled/src/main/Application.kt
+++ untitled/src/main/Application.kt
@@ -3,16 +3,22 @@ import kotlin.random.Random
 fun main() {
     val tmp = mutableListOf<Int>()
     while (tmp.size < 3) {
-        tmp.add(Random.nextInt(9))
+        tmp.add(Random.nextInt(1, 10))
     }
-
-    //val answer = tmp.joinToString("").toInt()
-    //print(answer)
+    val tmpArray = tmp.toIntArray()
 
     println("세 개의 숫자를 입력하세요(123처럼 빈칸 없이): ")
-    val input = readLine() ?: throw IllegalArgumentException("입력이 null입니다.")
+    val input = readLine()
+    val inputNum = mutableListOf<Int>()
 
-    val inputNum = input.takeIf { it.length == 3 }?.map { it.toString().toInt() }
-        ?: throw IllegalArgumentException("올바른 입력값이 아닙니다!")
-    //print(inputNum)
+    if (input == null || input.length != 3) {
+        throw IllegalArgumentException("올바른 입력값이 아닙니다!")
+    } else {
+        for (char in input) {
+            val num = char.toString().toInt()
+            inputNum.add(num)
+        }
+    }
+
+    check(inputNum.toIntArray(), tmpArray)
 }

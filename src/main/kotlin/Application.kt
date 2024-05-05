89be65188775124ab8c605943605cceb02e60bcf
dldjwls <dldjwls@naver.com>
2024-05-05 19:21:17 +0900

    feat: Added check playerNumber and threeNumber feature

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,5 +1,4 @@
 fun main() {
     val baseBallGame = Game()
-    baseBallGame.test()
 }
 
--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -21,12 +21,12 @@ class Game {
     private fun inputNumber(): MutableList<Int> {
         val playerNumbers = mutableListOf<Int>()
         print("숫자를 입력해 주세요 : ")
-        val input = readLine()
+        val input = readlnOrNull()
 
         //공백을 입력 했을 경우 예외처리
         input ?: throw IllegalArgumentException("숫자를 입력하지 않았습니다.")
         //세 자리 수 입력을 하지 않았을 경우 예외처리 ex) 12
-        if(input.length != 3) {
+        if (input.length != 3) {
             throw IllegalArgumentException("3개의 숫자를 입력해야 합니다.")
         }
 
@@ -37,11 +37,28 @@ class Game {
             }
             val numInt = num.toString().toInt()
             //중복된 숫자를 입력 했을 때 예외처리 ex) 122
-            if(playerNumbers.contains(numInt)) {
+            if (playerNumbers.contains(numInt)) {
                 throw IllegalArgumentException("중복된 숫자를 입력했습니다.")
             }
             playerNumbers.add(numInt)
         }
         return playerNumbers
     }
+
+    private fun checkNumber(
+        resultNumbers: MutableList<Int>, playerNumbers: MutableList<Int>
+    ) {
+        var strike = 0
+        var ball = 0
+        for ((index, value) in playerNumbers.withIndex()) {
+            if (resultNumbers.contains(value) && resultNumbers[index] == value) {
+                strike++
+            } else if (resultNumbers.contains(value)) {
+                ball++
+            } else {
+                continue
+            }
+        }
+    }
+
 }

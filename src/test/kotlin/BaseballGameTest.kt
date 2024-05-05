025ab234198155561b96c51f5668dc3f17d01259
유지예 <jiyeyu@yujiyeui-MacBookAir.local>
2024-05-06 05:00:10 +0900

    잘못된 입력 예외처리 테스트 케이스 구현

--- src/test/kotlin/BaseballGameTest.kt
+++ src/test/kotlin/BaseballGameTest.kt
@@ -4,5 +4,21 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 class BaseballGameTest {
+    @Test
+    @DisplayName("입력이 정상적으로 처리되는지 확인")
+    fun testUserInput() {
+        val game = BaseballGame()
+        val result = game.checkInput("123", game.targetNumbers)
+        assertEquals("낫싱", result)
+    }
 
+    @Test
+    @DisplayName("입력이 잘못된 경우 IllegalArgumentException 발생")
+    fun testInvalidInput() {
+        val game = BaseballGame()
+        val exception = assertThrows(IllegalArgumentException::class.java) {
+            game.checkInput("abc", game.targetNumbers)
+        }
+        assertEquals("Invalid input", exception.message)
+    }
 }

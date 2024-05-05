67fa236962263194b26cfed77f2f3e8fb2e8eaed
HyeonjiJang <jhg050908@gmail.com>
2024-05-05 16:11:37 +0900

    refactor: 게임 시작 및 종료 기능을 GameManager 클래스로 분리

--- src/main/kotlin/BaseBall.kt
+++ src/main/kotlin/BaseBall.kt
@@ -163,26 +163,36 @@ class InputChecker {
     }
 }
 
-fun main() {
-    val gameController = GameController()
+class GameManager {
+    fun manageGame(gameStateManager: GameStateManager) {
 
-    while (gameController.getGameState() != GameState.End) {
+        while (gameStateManager.getGameState() != GameState.End) {
+            gameStateManager.changeGameState(GameState.OnGoing)
 
-        gameController.changeGameState(GameState.OnGoing)
-        val computer = Computer()
-        val player = Player()
-        val inputChecker = InputChecker()
+            val computer = Computer(RandomNumberGenerator().setNumber())
+            val player = Player()
+            val inputChecker = InputChecker()
 
-        while (gameController.getGameState() == GameState.OnGoing) {
-            val userInput = player.getUserNumberInput(inputChecker)
-            computer.printResult(userInput, gameController)
-        }
-
-        val userInput = player.getUserGameStatusInput(inputChecker)
-
-        when (userInput) {
-            1 -> continue
-            2 -> gameController.changeGameState(GameState.End)
+            playGame(gameStateManager, player, computer, inputChecker)
+            val userInput = player.getUserGameStatusInput(inputChecker)
+            when (userInput) {
+                1 -> continue
+                2 -> gameStateManager.changeGameState(GameState.End)
+            }
         }
     }
+
+    private fun playGame(gameStateManager: GameStateManager, player: Player, computer: Computer, inputChecker: InputChecker) {
+        while (gameStateManager.getGameState() == GameState.OnGoing) {
+            val userInput = player.getUserNumberInput(inputChecker)
+            computer.printResult(userInput, gameStateManager)
+        }
+    }
+}
+
+fun main() {
+    val gameStateManager = GameStateManager()
+    val gameManager = GameManager()
+
+    gameManager.manageGame(gameStateManager)
 }

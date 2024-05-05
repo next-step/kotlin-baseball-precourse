8f879dcd0f84a7e64350d3a6c25a55b90fa04c88
ddeonseo <eunseo1370@gmail.com>
2024-05-05 23:44:51 +0900

    feat: enter number

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -5,3 +5,9 @@ fun generateNumber(): String {
     val numbers = (1..9).shuffled().take(3)
     return numbers.joinToString("")
 }
+
+fun enterNumber(): String {
+    print("숫자를 입력해 주세요 : ")
+    var inputNumber = readLine() ?: ""
+    return inputNumber
+}

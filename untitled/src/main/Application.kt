afbd82b6b1c29d7948c3d5159c6c277dc1925bad
DeveloperBaa <developer.baa.code@gmail.com>
2024-05-06 06:27:12 +0900

    IllegalArgumentException 발생시킴

--- untitled/src/main/Application.kt
+++ untitled/src/main/Application.kt
@@ -6,19 +6,13 @@ fun main() {
         tmp.add(Random.nextInt(9))
     }
 
-    val answer = tmp.joinToString("").toInt()
+    //val answer = tmp.joinToString("").toInt()
     //print(answer)
 
-    val inputNum = mutableListOf<Int>()
-
     println("세 개의 숫자를 입력하세요(123처럼 빈칸 없이): ")
-    val input = readLine()
+    val input = readLine() ?: throw IllegalArgumentException("입력이 null입니다.")
 
-    if (input != null && input.length == 3){
-        for (char in input) {
-            val num = char.toString().toInt()
-            inputNum.add(num)
-        }
-    }
+    val inputNum = input.takeIf { it.length == 3 }?.map { it.toString().toInt() }
+        ?: throw IllegalArgumentException("올바른 입력값이 아닙니다!")
     //print(inputNum)
 }

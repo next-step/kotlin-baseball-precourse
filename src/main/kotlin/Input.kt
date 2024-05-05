70bf9e222b8b9b3bfd23269082070e159597f6a7
Kjamm <91775639+Kjamm@users.noreply.github.com>
2024-05-05 23:53:19 +0900

    Input 함수 변경

--- src/main/kotlin/Input.kt
+++ src/main/kotlin/Input.kt
@@ -3,9 +3,8 @@ fun Input(): List<Int> {
         println("3개의 숫자를 입력해주세요 : ")
         val input = readLine() ?: throw IllegalArgumentException("입력이 잘못되었습니다.")
         val userInput = input.map { it.toString().toIntOrNull() ?: throw IllegalArgumentException("입력이 잘못되었습니다.") }
-        if (userInput.size != 3 || userInput.toSet().size != 3) {
+        if (userInput.size != 3 || userInput.toSet().size != 3 || userInput.any { it !in 1..9 }) {
             println("잘못된 입력입니다. 3개의 숫자를 1부터 9까지 중복 없이 입력해주세요.")
-            throw IllegalArgumentException("입력이 잘못되었습니다.")
         } else {
             return userInput
         }

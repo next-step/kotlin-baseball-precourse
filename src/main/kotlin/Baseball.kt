2bd62fb328e01d23343515ee6b80b5979f1018b2
ajang369 <jhjun99421@gmail.com>
2024-05-06 07:10:58 +0900

    fix : duplicate exception, index error
    숫자의 자리 수를 세는 length 변수를 잘못 사용하여 발생한 index error 수정
    입력한 숫자가 3자리가 아닐 때의 예외와 입력된 값이 null이라서 emptyList가 됐을 떄의 예외가 중복되어 수정함.

--- src/main/kotlin/Baseball.kt
+++ src/main/kotlin/Baseball.kt
@@ -3,8 +3,7 @@ import kotlin.random.Random
 fun main() {
     do {
         // 프로그램 첫 실행 시 조건없이 시작
-        val length = 3
-        val answerNumList = setRandomNumber(length)
+        val answerNumList = setRandomNumber()
 
         // 3 스트라이크가 될 때 까지 반복
         do {
@@ -12,7 +11,7 @@ fun main() {
             // 3개의 숫자 입력 예외 처리
             inputNumberException(inputNumList)
 
-            val (strike, ball) = countStrikeBall(answerNumList, inputNumList, length)
+            val (strike, ball) = countStrikeBall(answerNumList, inputNumList)
             printDecision(strike, ball)
         } while (strike != 3)
         println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
@@ -25,12 +24,11 @@ fun main() {
 }
 
 // 세자리 랜덤 숫자 생성
-fun setRandomNumber(length: Int): List<Int> {
+fun setRandomNumber(): List<Int> {
     val randomNum = mutableListOf<Int>()
-    while (randomNum.size < length) {
+    while (randomNum.size < 3) {
         val num = Random.nextInt(1, 9)
-        if (!randomNum.contains(num))
-            randomNum.add(num)
+        if (!randomNum.contains(num)) randomNum.add(num)
     }
     return randomNum
 }
@@ -45,15 +43,13 @@ fun inputNumber(): MutableList<Int> {
 }
 
 // 스트라이크, 볼 카운트
-fun countStrikeBall(randomList: List<Int>, numList: List<Int>, length: Int): Pair<Int, Int> {
+fun countStrikeBall(randomList: List<Int>, numList: List<Int>): Pair<Int, Int> {
     var strike: Int = 0
     var ball: Int = 0
-    for (i in 0..length) {
+    for (i in 0 until 3) {
         if (numList[i] in randomList) {
-            if (numList[i] == randomList[i])
-                strike += 1
-            else
-                ball += 1
+            if (numList[i] == randomList[i]) strike += 1
+            else ball += 1
         }
     }
     return Pair(strike, ball)
@@ -82,10 +78,11 @@ fun inputNumberException(numList: List<Int>) {
     }
 
     when {
-        (numList.size != 3) -> throw IllegalArgumentException("잘못된 입력: 3개의 숫자가 아닙니다.")
+        (numList.size != 3) ->
+            if (numList.isEmpty()) throw IllegalArgumentException("잘못된 입력: 입력된 값이 없습니다.")
+            else throw IllegalArgumentException("잘못된 입력: 3개의 숫자가 아닙니다.")
         (numList.any { it == 0 }) -> throw IllegalArgumentException("잘못된 입력: 숫자 0은 입력할 수 없습니다.")
         (isDuplication) -> throw IllegalArgumentException("잘못된 입력: 중복된 숫자가 있습니다.")
-        (numList.isEmpty()) -> throw IllegalArgumentException("잘못된 입력: 입력된 값이 없습니다.")
     }
 }
 

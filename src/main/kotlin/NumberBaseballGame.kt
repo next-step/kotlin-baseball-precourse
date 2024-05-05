d5dcb8db961abfba1fecfa9e4e8b5e886ebda58f
jooiss <jusong513@pusan.ac.kr>
2024-05-06 01:28:05 +0900

    feat: add a function to compare computer number with user number

--- src/main/kotlin/NumberBaseballGame.kt
+++ src/main/kotlin/NumberBaseballGame.kt
@@ -4,6 +4,7 @@ fun main() {
     val computerInput = generateComputerNumber()
     val userInput = inputUserNumber()
     isValidInput(userInput)
+    val (strikes, balls) = calculateResult(computerInput, userInput)
 }
 
 fun generateComputerNumber(): String {
@@ -26,4 +27,17 @@ fun isValidInput(input: String) {
     if (input.length != 3 || !input.all { it.isDigit() }) {
         throw IllegalArgumentException("1부터 9까지 서로 다른 수로 이루어진 3자리의 수를 입력해주세요.")
     }
+}
+
+fun calculateResult(computerInput: String, userInput: String): Pair<Int, Int> {
+    var strikes = 0
+    var balls = 0
+    for (i in computerInput.indices) {
+        if (computerInput[i] == userInput[i]) {
+            strikes++
+        } else if (computerInput.contains(userInput[i])) {
+            balls++
+        }
+    }
+    return Pair(strikes, balls)
 }

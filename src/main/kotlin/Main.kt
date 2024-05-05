a5a6cffd803c199a00929d0e87c0b0fc87d9ae26
kyleidea1 <99781357+kyleidea1@users.noreply.github.com>
2024-05-05 21:02:43 +0900

    Main.kt 생성
    숫자야구 프로그램을 실행할 Main.kt 생성 및 핵심 함수 정의

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+package baseball
+
+fun main() {
+    // 프로그램 실행부
+}
+
+fun generateAnswer(): String {
+    // 정답 수 생성
+}
+
+fun checkInputIfValid(input: String): Boolean {
+    // 적절한 입력인 지 확인
+}
+
+fun evaluate(input: String, answer: String): String {
+    // 입력에 대한 평가 및 결과 출력
+}

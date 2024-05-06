c13378872b21c19aa9b84aa0706805d20882048f
fiveJinw <fiveJinw1124@gmail.com>
2024-05-06 11:09:31 +0900

    feat: add feature to manage game flow

--- src/main/kotlin/GameManager.kt
+++ src/main/kotlin/GameManager.kt
@@ -8,4 +8,37 @@ class GameManager {
         computer = Computer()
         isGameEnd = false
     }
+
+    private fun startGame() {
+        while (isGameRestart == 1) {
+            initGame()
+            playGameContinue()
+        }
+    }
+
+    private fun playGameContinue() {
+        while (!isGameEnd) {
+            val inputNumber: List<Int> = userInterface.inputThreeNumber()
+            val strikeAndBallCountList: List<Int> =
+                computer.compareInputWithCorrectNumber(inputNumber)
+            userInterface.printResult(strikeAndBallCountList)
+            checkGameEnd(strikeAndBallCountList)
+        }
+    }
+
+    private fun checkGameEnd(strikeAndBallCountList: List<Int>) {
+        val strikeIndex = 0
+        if (strikeAndBallCountList[strikeIndex] == 3) {
+            isGameEnd = true
+            isGameRestart = userInterface.inputGameRestart()
+        }
+    }
+
+    fun startGamePlayWithExceptionChecker() {
+        try {
+            startGame()
+        } catch (e: IllegalArgumentException) {
+            println("잘못된 입력 감지. ${e.message}, 게임을 종료합니다.")
+        }
+    }
 }

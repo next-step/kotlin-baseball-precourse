c77b3669d7bdf7207c3d4dd400a38faa7fadf228
fiveJinw <fiveJinw1124@gmail.com>
2024-05-06 10:55:35 +0900

    feat: add feature to input three number

--- src/main/kotlin/UserInterface.kt
+++ src/main/kotlin/UserInterface.kt
@@ -17,4 +17,54 @@ class UserInterface() {
             "${strikeAndBallCountList[strikeIndex]}스트라이크"
         } else "${strikeAndBallCountList[strikeIndex]}스트라이크 ${strikeAndBallCountList[ballIndex]}볼"
     }
+
+    fun inputThreeNumber(): List<Int> {
+        print("숫자를 입력해 주세요 : ")
+        val number = readln()
+        return changeNumber(number)
+    }
+
+    fun changeNumber(stringNumber: String): List<Int> {
+        val intNumber = inputStrToIntNumber(stringNumber)
+        val listNumber = listOf<Int>()
+
+        checkHundredNumber(intNumber)
+        val newListNumber = listNumber + intNumber / 100 + (intNumber % 100) / 10 + (intNumber % 10)
+        checkContainZero(newListNumber)
+        checkDuplicateNum(newListNumber)
+        return newListNumber
+    }
+
+    fun inputStrToIntNumber(stringNumber: String): Int {
+        try {
+            return stringNumber.toInt()
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException("숫자가 아닌 값을 입력 했습니다. : $stringNumber")
+        }
+    }
+
+    fun checkHundredNumber(intNumber: Int) {
+        if (isNotHundredNumber(intNumber)) throw IllegalArgumentException("허용된 범위의 수가 아닙니다.: ${intNumber}")
+    }
+
+    fun isNotHundredNumber(intNumber: Int): Boolean {
+        return !((intNumber >= 123) and (intNumber <= 987))
+    }
+
+    fun checkContainZero(listNumber: List<Int>) {
+        if (isContainZero(listNumber)) throw IllegalArgumentException("0은 포함할 수 없습니다 : ${listNumber}")
+    }
+
+    fun isContainZero(listNumber: List<Int>): Boolean {
+        return listNumber.contains(0)
+    }
+
+    fun checkDuplicateNum(listNumber: List<Int>) {
+        if (isDuplicateNum(listNumber)) throw IllegalArgumentException("중복 되지 않은 수여야 합니다 : ${listNumber}")
+    }
+
+    fun isDuplicateNum(listNumber: List<Int>): Boolean {
+        val setNumber = listNumber.toSet()
+        return setNumber.size < 3
+    }
 }
--- src/test/kotlin/UserInterfaceTest.kt
+++ src/test/kotlin/UserInterfaceTest.kt
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import kotlin.IllegalArgumentException
 
 class UserInterfaceTest() {
@@ -14,5 +15,12 @@ class UserInterfaceTest() {
         assertThat(userInterface.replaceCountListWithString(listOf(0, 0))).isEqualTo("낫싱")
         assertThat(userInterface.replaceCountListWithString(listOf(1, 0))).isEqualTo("1스트라이크")
         assertThat(userInterface.replaceCountListWithString(listOf(0, 1))).isEqualTo("1볼")
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1234", "402", "133", "12", "cake"])
+    fun checkNumberTest(string: String) {
+        val userInterface: UserInterface = UserInterface()
+        assertThrows<IllegalArgumentException> { userInterface.changeNumber(string) }
     }
 }

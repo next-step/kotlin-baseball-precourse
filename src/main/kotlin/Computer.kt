9882c08dddd4c8fda5b172f48800e28dde54528a
fiveJinw <fiveJinw1124@gmail.com>
2024-05-06 10:05:35 +0900

    feat: add feature to count strike and ball

--- src/main/kotlin/Computer.kt
+++ src/main/kotlin/Computer.kt
@@ -1,5 +1,5 @@
 class Computer constructor() {
-    var correctNumberList:List<Int> = listOf<Int>()
+    var correctNumberList: List<Int> = listOf<Int>()
 
     init {
         generateRandomNumber()
@@ -12,5 +12,28 @@ class Computer constructor() {
                 correctNumberList = correctNumberList + randomNumber
             }
         }
+    }
+
+    fun compareInputWithCorrectNumber(inputAnswer: List<Int>): List<Int> {
+        var numberOfStrike: Int = 0
+        var numberOfBall: Int = 0
+        for (i in 0..2) {
+            numberOfStrike += checkStrikes(i, inputAnswer[i], correctNumberList)
+            numberOfBall += checkBalls(i, inputAnswer[i], correctNumberList)
+        }
+        return listOf(numberOfStrike, numberOfBall)
+    }
+
+    fun checkStrikes(index: Int, inputNumber: Int, targetList: List<Int>): Int {
+        return if (targetList[index] == inputNumber) 1
+        else 0
+    }
+
+    fun checkBalls(index: Int, inputNumber: Int, targetList: List<Int>): Int {
+        return if ((checkStrikes(
+                index, inputNumber, targetList
+            ) == 0) and targetList.contains(inputNumber)
+        ) 1
+        else 0
     }
 }
--- src/test/kotlin/ComputerTest.kt
+++ src/test/kotlin/ComputerTest.kt
@@ -11,4 +11,18 @@ class ComputerTest() {
         val computer: Computer = Computer()
         assertThat(computer.correctNumberList.size).isEqualTo(3)
     }
+
+    @Test
+    @DisplayName("스트라이크를 잘 판단하는 지 확인")
+    fun checkStrikesTest() {
+        val computer: Computer = Computer()
+        assertThat(computer.checkStrikes(0, 1, listOf(1, 2, 3))).isEqualTo(1)
+    }
+
+    @Test
+    @DisplayName("볼을 잘 판단하는지 확인")
+    fun checkBallsTest() {
+        val computer: Computer = Computer()
+        assertThat(computer.checkBalls(2, 1, listOf(1, 2, 3))).isEqualTo(1)
+    }
 }

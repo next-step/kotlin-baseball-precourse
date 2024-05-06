ec7360d128290d755b9bbd0bb33c14e591bbdd3e
narim <0018823nmn@naver.com>
2024-05-06 11:40:24 +0900

    feat: User 클래스에 입력받은 숫자를 배열로 바꾸는 change() 함수 추가

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,36 +4,50 @@ class Com constructor(){
 
     var randomNumber: IntArray = IntArray(3)
 
+
     fun makeNumber() {
         for (i in 0..2) randomNumber[i] = (1..9).random()
         println("랜덤한 숫자: ${randomNumber.joinToString()}")
     }
+
+
 
 }
 
 class User constructor(){
 
     var inputNumber: Int = 0
+    var inputArray: IntArray = IntArray(3,{0})
 
     fun input() {
         println("1~9 사이의 숫자 3개를 공백없이 입력하세요:")
         try {
             inputNumber = readLine()!!.toInt()
-            if (inputNumber !in 111..999) {
-                throw IllegalArgumentException("1~9 사이의 숫자를 3개 입력하세요.")
-            }
         } catch (e: NumberFormatException) {
             throw IllegalArgumentException("올바른 숫자를 입력하세요.")
+        }
+        if (inputNumber !in 111..999) {
+            throw IllegalArgumentException("1~9 사이의 숫자를 3개 입력하세요.")
         }
     }
 
     fun print() {
         println("입력한 숫자: $inputNumber")
     }
+
+    fun change() {
+        inputArray[0]= inputNumber/100
+        inputArray[1] = (inputNumber / 10) % 10
+        inputArray[2] = inputNumber%10
+        println("변환된 숫자 배열: ${inputArray.joinToString()}")
+    }
 }
 
 fun main() {
+    val com = Com()
+    com.makeNumber()
     val user = User()
     user.input()
     user.print()
+    user.change()
 }

1d93a046c856735665fa7809717688c0d52a57cd
narim <0018823nmn@naver.com>
2024-05-06 11:27:01 +0900

    feat: User class, input 함수와 에외처리, print 함수 추가

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,6 @@
 import kotlin.random.Random
 
-class Play {
+class Com constructor(){
 
     var randomNumber: IntArray = IntArray(3)
 
@@ -8,9 +8,32 @@ class Play {
         for (i in 0..2) randomNumber[i] = (1..9).random()
         println("랜덤한 숫자: ${randomNumber.joinToString()}")
     }
+
+}
+
+class User constructor(){
+
+    var inputNumber: Int = 0
+
+    fun input() {
+        println("1~9 사이의 숫자 3개를 공백없이 입력하세요:")
+        try {
+            inputNumber = readLine()!!.toInt()
+            if (inputNumber !in 111..999) {
+                throw IllegalArgumentException("1~9 사이의 숫자를 3개 입력하세요.")
+            }
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException("올바른 숫자를 입력하세요.")
+        }
+    }
+
+    fun print() {
+        println("입력한 숫자: $inputNumber")
+    }
 }
 
 fun main() {
-    val game = Play()
-    game.makeNumber()
+    val user = User()
+    user.input()
+    user.print()
 }

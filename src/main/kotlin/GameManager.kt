28c8a318eae527bd46206d0fc0f3b5801141bee9
이지은 <jieun7056@naver.com>
2024-05-06 09:52:25 +0900

    feat: 나의 숫자 채점하기

--- src/main/kotlin/GameManager.kt
+++ src/main/kotlin/GameManager.kt
@@ -9,9 +9,41 @@ class GameManager constructor(){
         println(this.computerNumber)
     }
 
+    fun guess(guessNumber: String) {
+        var (strikes, balls) = mark(guessNumber)
+        printScores(strikes, balls)
+    }
+
+    private fun mark(guessNumber: String): Pair<Int, Int> {
+        var strikes = 0
+        var balls = 0
+
+        guessNumber.forEachIndexed { index, c ->
+            if (c == computerNumber[index]) {
+                // 같은 위치에 같은 숫자가 있는 경우, 스트라이크
+                strikes++
+            } else if (c in computerNumber) {
+                // 다른 위치에 숫자가 있는 경우, 볼
+                balls++
+            }
+        }
+        return Pair(strikes, balls)
+    }
+
+    private fun printScores(strikes: Int, balls: Int) {
+        when {
+            strikes != 0 && balls != 0 -> println("${balls}볼 ${strikes}스트라이크")
+            strikes != 0 -> println("${strikes}스트라이크")
+            balls != 0 -> println("${balls}볼")
+            else -> println("낫싱")
+        }
+    }
+
+
 }
 
 fun main() {
     val gameManager: GameManager = GameManager()
     gameManager.setComputerNumber()
+    gameManager.guess("123")
 }

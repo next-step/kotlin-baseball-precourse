46d47dd112a2963d399b533b14aa3150f48b376d
SHEEN <normengdie@pusan.ac.kr>
2024-05-10 00:55:12 +0900

    lines sort

--- src/main/worldbaseball.kt
+++ src/main/worldbaseball.kt
@@ -1,4 +1,4 @@
-fun main(){
+fun main() {
     while(true){
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
         val game = readLine()!!
@@ -14,20 +14,7 @@ fun main(){
         }
     }
 }
-fun inputUserNumber(randomNumberInFunction : String){
-    while(true){
-        println("숫자를 입력해 주세요:")
-        var userNumberInFunction : String = readLine()!!
-        isItPossibleToUse(userNumberInFunction)
-        val result = strikeAndBallDecisions(userNumberInFunction,randomNumberInFunction)
-        if (result == true){
-            break
-        }else if (result == false){
-            continue
-        }
 
-    }
-}
 fun strikeAndBallDecisions(checkUserNumber: String, checkRandomNumber : String) : Boolean{ //스트라이크 볼 판정
     var ball : Int = 0
     var strike : Int = 0
@@ -45,40 +32,6 @@ fun strikeAndBallDecisions(checkUserNumber: String, checkRandomNumber : String) 
     return  printResult(strike,ball)
 
 }
-fun userNumberCheck(numbers : String) : Boolean{
-    val numbersList = numbers.toMutableList()
-    val checkedNumbersList = numbersList.distinct()
-    if (checkedNumbersList.size == 3){
-        return true
-
-    }else{
-        return false
-    }
-
-}
-fun isItPossibleToUseUserNumber(userNumberInFunction:String){
-    if(userNumberInFunction.length != 3 || userNumberCheck(userNumberInFunction) == false){
-        throw IllegalArgumentException("A number must be 100~999 ")
-    }
-}
-
-fun createdRandomNumber() : String { // 난수 생성 함수
-    while(true) {
-        val numberRange = (100..999)
-        if(withoutOverlapping(numberRange).size == 3){
-            return withoutOverlapping(numberRange).joinToString("")}
-        else{
-            continue
-        }
-    }
-
-}
-fun withoutOverlapping(numberRange: IntRange) : List<Char> {
-    val randomNumber = numberRange.random().toString()
-    val numberList = randomNumber.toMutableList()
-    val checkedNumberList = numberList.distinct()
-    return checkedNumberList
-}
 fun printResult(strike : Int, ball : Int) : Boolean {
     if (strike == 3) {
         println("3스트라이크")
@@ -95,4 +48,56 @@ fun printResult(strike : Int, ball : Int) : Boolean {
     }
     return false
 
+}
+
+fun inputUserNumber(randomNumberInFunction : String){
+    while(true){
+        println("숫자를 입력해 주세요:")
+        var userNumberInFunction : String = readLine()!!
+        isItPossibleToUse(userNumberInFunction)
+        val result = strikeAndBallDecisions(userNumberInFunction,randomNumberInFunction)
+        if (result == true){
+            break
+        }else if (result == false){
+            continue
+        }
+
+    }
+}
+
+
+fun createdRandomNumber() : String { // 난수 생성 함수
+    while(true) { // 무한 루프 시작
+        val numberRange = (100..999) // 100 ~ 999까지 범위 설정
+        if(withoutOverlapping(numberRange).size == 3){ // 중복 제거 후 리스트 사이즈 체크 3이면 세개의 숫자가 중복 없이 생성 됨을 알 수 있음
+            return withoutOverlapping(numberRange).joinToString("")} //리스트에 있는 문자들을 합쳐서 문자열로 반환
+        else{
+            continue // 숫자가 3이 아닐 경우 다시 무한루프로 돌아감
+        }
+    }
+
+}
+
+fun withoutOverlapping(numberRange: IntRange) : List<Char> { // 중복 제거 함수
+    val randomNumber = numberRange.random().toString() //범위 안에서 난수 생성후 문자열로 치환
+    val numberList = randomNumber.toMutableList() //문자열를 MutableList로 변경
+    val checkedNumberList = numberList.distinct() // 리스트안에 있는 중복 제거
+    return checkedNumberList // 리스트 반환
+}
+
+fun userNumberCheck(numbers : String) : Boolean{
+    val numbersList = numbers.toMutableList()
+    val checkedNumbersList = numbersList.distinct()
+    if (checkedNumbersList.size == 3){
+        return true
+
+    }else{
+        return false
+    }
+
+}
+fun isItPossibleToUseUserNumber(userNumberInFunction:String){
+    if(userNumberInFunction.length != 3 || userNumberCheck(userNumberInFunction) == false){
+        throw IllegalArgumentException("A number must be 100~999 ")
+    }
 }

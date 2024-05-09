40d24904f9ba72acdc67bb2225f727a5714e8f04
SHEEN <normengdie@pusan.ac.kr>
2024-05-10 00:52:17 +0900

    made createdRandomNumber and withoutOverlapping function

--- src/main/worldbaseball.kt
+++ src/main/worldbaseball.kt
@@ -4,3 +4,21 @@ fun main(){
 
 
 }
+
+fun createdRandomNumber() : String { // 난수 생성 함수
+    while(true) {
+        val numberRange = (100..999)
+        if(withoutOverlapping(numberRange).size == 3){
+            return withoutOverlapping(numberRange).joinToString("")}
+        else{
+            continue
+        }
+    }
+
+}
+fun withoutOverlapping(numberRange: IntRange) : List<Char> {
+    val randomNumber = numberRange.random().toString()
+    val numberList = randomNumber.toMutableList()
+    val checkedNumberList = numberList.distinct()
+    return checkedNumberList
+}

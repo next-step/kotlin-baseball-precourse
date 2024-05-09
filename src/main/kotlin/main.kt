ba20ece978d32feda123575a178ed21e7b020b24
leeghy <epuy109@gmail.com>
2024-05-09 20:30:25 +0900

    userInput 함수

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -22,7 +22,17 @@ fun random() { //랜덤 함수: 랜덤으로 숫자 세 개를 생성한다.
 }
 
 fun userInput() {
+    val userNumber: Array<Int?> = arrayOfNulls<Int>(3)
+    print("숫자 세 개를 입력해주세요: ")
+    val inputNumber: String = readLine() ?: throw IllegalArgumentException("입력이 없습니다.")
 
+    if (inputNumber.length != 3 || !inputNumber.all { it.isDigit() }) {
+        throw IllegalArgumentException("다시 입력해주세요.")
+    }
+
+    userNumber[0] = inputNumber.substring(0, 1).toInt()
+    userNumber[1] = inputNumber.substring(1, 2).toInt()
+    userNumber[2] = inputNumber.substring(2, 3).toInt()
 }
 
 fun judgement() {

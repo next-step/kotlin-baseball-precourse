d2a75ca6756aca67afeccba359571f06d85cb9b4
HyeonjiJang <jhg050908@gmail.com>
2024-05-10 00:24:22 +0900

    update: 예외 처리 방식 수정 - 요구 사항에 맞추어 IllegalArgumentException 발생시키도록 변경

--- src/main/kotlin/BaseBall.kt
+++ src/main/kotlin/BaseBall.kt
@@ -80,13 +80,8 @@ class Player {
 
     fun getUserNumberInput(inputChecker: InputChecker): Array<Int> {
         print("숫자를 입력해 주세요 : ")
-        var userInput: String = getUserInput()
-        var inputNumberState: Boolean = inputChecker.checkThreeDigitInput(userInput)
-        while (!inputNumberState) {
-            print("숫자를 입력해 주세요 : ")
-            userInput = getUserInput()
-            inputNumberState = inputChecker.checkThreeDigitInput(userInput)
-        }
+        val userInput: String = getUserInput()
+        inputChecker.checkThreeDigitInput(userInput)
 
         return getArrayInput(userInput)
     }
@@ -102,64 +97,60 @@ class Player {
 
     fun getUserGameStatusInput(inputChecker: InputChecker): Int {
         println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-        var userInput: String = getUserInput()
-        var inputNumberState: Boolean = inputChecker.checkOneDigitInput(userInput)
-        while (!inputNumberState) {
-            userInput = getUserInput()
-            inputNumberState = inputChecker.checkOneDigitInput(userInput)
-        }
+        val userInput: String = getUserInput()
+        inputChecker.checkOneDigitInput(userInput)
 
         return userInput.toInt()
     }
 }
 
 class InputChecker {
-    fun checkThreeDigitInput(userInput: String): Boolean {
-        return isInt(userInput) && isThreeDigit(userInput) && notDuplicated(userInput)
+    fun checkThreeDigitInput(userInput: String) {
+        isInt(userInput)
+        isThreeDigit(userInput)
+        notDuplicated(userInput)
     }
 
-    fun checkOneDigitInput(userInput: String): Boolean {
-        return isInt(userInput) && isOneOrTwo(userInput)
+    fun checkOneDigitInput(userInput: String) {
+        isInt(userInput)
+        isOneOrTwo(userInput)
     }
 
-    private fun isOneOrTwo(userInput: String): Boolean {
+    private fun isOneOrTwo(userInput: String) {
         val num = userInput.toInt()
         if (num == 1 || num == 2) {
-            return true
+            return
         }
-        println("입력값이 1 또는 2가 아닙니다.")
-        return false
+        throw IllegalArgumentException ("입력값이 1 또는 2가 아닙니다.")
     }
 
-    private fun isInt(userInput: String): Boolean {
-        return try {
+    private fun isInt(userInput: String) {
+        try {
             userInput.toInt()
-            true
+            return
         } catch (e: NumberFormatException) {
-            println("입력값이 정수가 아닙니다.")
-            false
+            throw IllegalArgumentException ("입력값이 정수가 아닙니다.")
         }
     }
 
-    private fun isThreeDigit(userInput: String): Boolean {
+    private fun isThreeDigit(userInput: String) {
         if (userInput.length == 3) {
-            return true
+            return
         }
-        println("입력값이 세 자리수가 아닙니다.")
-        return false
+        throw IllegalArgumentException ("입력값이 세 자리수가 아닙니다.")
     }
 
-    private fun notDuplicated(userInput: String): Boolean {
+    private fun notDuplicated(userInput: String){
         if (userInput[0] == userInput[1]) {
-            return false
+            throw IllegalArgumentException ("입력값에 중복이 존재합니다.")
         }
         if (userInput[1] == userInput[2]) {
-            return false
+            throw IllegalArgumentException ("입력값에 중복이 존재합니다.")
         }
         if (userInput[2] == userInput[0]) {
-            return false
+            throw IllegalArgumentException ("입력값에 중복이 존재합니다.")
         }
-        return true
+        return
     }
 }
 

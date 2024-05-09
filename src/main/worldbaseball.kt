ee3409a3fafa5d65728d9afada876c3109e9e6a6
SHEEN <normengdie@pusan.ac.kr>
2024-05-10 00:52:40 +0900

    made printResult function

--- src/main/worldbaseball.kt
+++ src/main/worldbaseball.kt
@@ -22,3 +22,20 @@ fun withoutOverlapping(numberRange: IntRange) : List<Char> {
     val checkedNumberList = numberList.distinct()
     return checkedNumberList
 }
+fun printResult(strike : Int, ball : Int) : Boolean {
+    if (strike == 3) {
+        println("3스트라이크")
+        println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+        return true
+    } else if (ball == 0 && strike == 0) {
+        println("낫싱")
+    } else if (ball == 0 && strike != 0) {
+        println("${strike}스트라이크")
+    } else if (ball != 0 && strike == 0) {
+        println("${ball}볼")
+    } else {
+        println("${ball}볼 ${strike}스트라이크")
+    }
+    return false
+
+}

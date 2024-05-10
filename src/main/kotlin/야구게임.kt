ff673c6a1e136c71b361ca35b36f04eb53e68886
heonniy <dkgus7535@naver.com>
2024-05-10 09:30:17 +0900

    kotlin으로 수정하여 재제출

---
+++ src/main/kotlin/야구게임.kt
@@ -0,0 +1,91 @@
+import kotlin.random.Random
+
+fun createNum(): MutableList<Int> {
+    val numList: MutableList<Int> = mutableListOf()
+    while (numList.size < 3) {
+        val num: Int = Random.nextInt(1, 10)
+        if (!numList.contains(num)) {
+            numList.add(num)
+        }
+    }
+    return numList
+}
+
+fun checkNum(): MutableList<Int>? {
+    val checkNumList: MutableList<Int> = mutableListOf()
+    print("숫자를 입력해 주세요 : ")
+    val input = readLine()?.trim() ?: return null
+    val num = input.toIntOrNull() ?: return null
+
+    if (input.length != 3) return null
+
+    val hundredDigit = num / 100
+    val tenDigit = (num / 10) % 10
+    val oneDigit = num % 10
+
+    if (hundredDigit == tenDigit || hundredDigit == oneDigit || tenDigit == oneDigit) {
+        println("중복된 숫자는 입력할 수 없습니다.")
+        return null
+    }
+
+    checkNumList.add(hundredDigit)
+    checkNumList.add(tenDigit)
+    checkNumList.add(oneDigit)
+
+    return checkNumList
+}
+
+fun isCorrect(list1: MutableList<Int>, list2: MutableList<Int>?): Boolean {
+    if (list2 == null) return false
+    var strike = 0
+    var ball = 0
+
+    for (i in 0..2) {
+        if (list1[i] == list2[i])
+            strike += 1
+        else if (list2.contains(list1[i]))
+            ball += 1
+    }
+
+    when {
+        strike == 3 -> {
+            println("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+            return true
+        }
+        strike > 0 && ball > 0 -> println("${ball}볼 ${strike}스트라이크")
+        strike > 0 && ball <= 0 -> println("${strike}스트라이크")
+        strike <= 0 && ball > 0 -> println("${ball}볼")
+        else -> println("낫싱")
+    }
+    return false
+}
+
+fun newGameSelect(): Boolean {
+    println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    val restart = readLine()?.toIntOrNull()
+    return when (restart) {
+        1 -> true
+        else -> false
+    }
+}
+
+fun main() {
+    var continueGame = true
+    val computerList = createNum()
+
+    while (continueGame) {
+        val playerList = checkNum()
+        if (playerList == null) {
+            println("잘못된 입력입니다.")
+            continue
+        }
+        val isGameEnd = isCorrect(computerList, playerList)
+        if (isGameEnd) {
+            continueGame = newGameSelect()
+            if (continueGame) {
+                main()
+                return
+            }
+        }
+    }
+}

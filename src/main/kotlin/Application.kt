bee5cd013f9cea23b634c2af60e1b1dd51d26517
DeveloperBaa <developer.baa.code@gmail.com>
2024-05-10 16:09:34 +0900

    컴퓨터가 생각하고 있는 숫자와 게임 플레이어가 입력한 숫자를 비교

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,10 +1,16 @@
 import kotlin.random.Random
 
 fun main(){
-    //val answer = setComputer()
-    //println("Computer : ${answer.joinToString()}")
-    val guess = getPlayerInput()
-    println("Game Player : ${guess.joinToString()}")
+    var continueGame = true
+    val answer = setComputer()
+    println("Computer : ${answer.joinToString()}")
+
+    while(continueGame){
+        val guess = getPlayerInput()
+        println("Game Player : ${guess.joinToString()}")
+        val result = compareAnswerGuess(answer,guess)
+    }
+
 }
 
 //컴퓨터가 1에서 9까지 서로 다른 임의의 수 3개 선택
@@ -28,3 +34,24 @@ fun getPlayerInput(): IntArray {
     return guess
 }
 
+//컴퓨터가 생각하고 있는 숫자와 게임 플레이어가 입력한 숫자를 비교
+fun compareAnswerGuess(answer: IntArray, guess: IntArray): String {
+    var strikes = 0
+    var balls = 0
+
+    for (i in answer.indices) {
+        if (answer[i] == guess[i]) {
+            strikes++
+        } else if (answer.contains(guess[i])) {
+            balls++
+        }
+    }
+
+    return when {
+        strikes == 3 -> "3 strikes"
+        strikes > 0 && balls > 0 -> "$strikes strike, $balls ball"
+        strikes > 0 -> "$strikes strike"
+        balls > 0 -> "$balls ball"
+        else -> "nothing"
+    }
+}
